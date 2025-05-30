b6dc1331cd29431af214e07558a6df6df2080031
redflameken <kabalacaba@gmail.com>
2025-05-30 12:19:52 +0800

    Revert to the using java.util.Base64 after bumping android api to 25

--- app/src/main/java/com/realeyez/trademart/util/Encoder.java
+++ app/src/main/java/com/realeyez/trademart/util/Encoder.java
@@ -1,23 +1,23 @@
 package com.realeyez.trademart.util;
 
-import android.util.Base64;
+import java.util.Base64;
 
 public class Encoder {
 
     public static String encodeBase64(String data){
-        return Base64.encodeToString(data.getBytes(), Base64.DEFAULT);
+        return Base64.getEncoder().encodeToString(data.getBytes());
     }
     
     public static String encodeBase64(byte[] data){
-        return Base64.encodeToString(data, Base64.DEFAULT);
+        return Base64.getEncoder().encodeToString(data);
     }
     
     public static byte[] decodeBase64(String data){
-        return Base64.decode(data.getBytes(), Base64.DEFAULT);
+        return Base64.getDecoder().decode(data);
     }
 
     public static String decodeBase64String(String data){
-        return new String(Base64.decode(data, Base64.DEFAULT));
+        return new String(Base64.getDecoder().decode(data));
     }
 
 }
