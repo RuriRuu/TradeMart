f850ea777ca0e44346c9d106d207ad8639607979
RedFlameKen <nettolacaba@gmail.com>
2025-06-02 21:18:34 +0800

    Add more dialogs

--- app/src/main/java/com/realeyez/trademart/util/Dialogs.java
+++ app/src/main/java/com/realeyez/trademart/util/Dialogs.java
@@ -5,12 +5,25 @@ import android.content.Context;
 
 public class Dialogs {
 
-    public static void showMessageDialog(String message, Context context){
+    public static void showDialog(String title, String message, Context context){
         AlertDialog.Builder builder = new AlertDialog.Builder(context)
-            .setTitle("Message")
+            .setTitle(title)
             .setMessage(message);
         builder.setPositiveButton("OK", (dialog, which) -> dialog.dismiss());
         AlertDialog dialog = builder.create();
         dialog.show();
     }
+
+    public static void showMessageDialog(String message, Context context){
+        showDialog("Info", message, context);
+    }
+
+    public static void showWarningDialog(String message, Context context){
+        showDialog("Warning", message, context);
+    }
+
+    public static void showErrorDialog(String message, Context context){
+        showDialog("Error", message, context);
+    }
+
 }
