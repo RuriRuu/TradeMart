a273b19d1fc8db6b34705115c4b360df1395d451
CerysKulla <elpheltonline@gmail.com>
2025-06-09 17:58:54 +0800

    Changed Job List Editing to just Job List to avoid confusion. Also added reporting prompt v1

--- app/src/main/java/com/realeyez/trademart/JobListEditing.java
+++ app/src/main/java/com/realeyez/trademart/JobListEditing.java
@@ -16,7 +16,7 @@ public class JobListEditing extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_job_list_editing);
+        setContentView(R.layout.activity_job_list);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.activity_job_list_editing), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
