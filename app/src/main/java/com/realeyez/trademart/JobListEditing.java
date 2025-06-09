c17bf8068a2aa00907e5d8593a258a1a8baada33
CerysKulla <elpheltonline@gmail.com>
2025-06-09 21:02:27 +0800

    report prompt added to job list. Also changed reporting prompt by only a bit

--- app/src/main/java/com/realeyez/trademart/JobListEditing.java
+++ app/src/main/java/com/realeyez/trademart/JobListEditing.java
@@ -1,14 +1,23 @@
 package com.realeyez.trademart;
 
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.RatingBar;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Objects;
 
 public class JobListEditing extends AppCompatActivity {
 
@@ -21,6 +30,24 @@ public class JobListEditing extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        ImageButton exitJobList = findViewById(R.id.exitJobListView);
+        ImageButton reportJobList = findViewById(R.id.reportButton);
+
+        exitJobList.setOnClickListener(view -> {
+            Intent explicitActivity = new Intent(JobListEditing.this, LoginPageActivity.class);
+            startActivity(explicitActivity);
+        });
+
+        reportJobList.setOnClickListener(view -> {
+            Dialog dialog = new Dialog(this);
+            dialog.setContentView(R.layout.reporting_prompt);
+            dialog.show();
+                Objects.requireNonNull(dialog.getWindow()).setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+                dialog.findViewById(R.id.cancelReportPrompt).setOnClickListener(v -> dialog.dismiss());
+
         });
 
         Spinner JobTiers = findViewById(R.id.jobListTiers);
